5bfbe85196ad2507265bd0a6e3211e7a399a236c
hglf <hgleifeng@foxmail.com>
2018-05-09 22:00:14 +0800

    fix

--- ss-priproxy/entrypoint.sh
+++ ss-priproxy/entrypoint.sh
@@ -4,7 +4,7 @@ L_PORT=${L_PORT:-"6444"}
 F_PORT=${F_PORT:-"6443"}
 
 cp /etc/privoxy/configbak /etc/privoxy/config
-echo listen-address 0.0.0.0:${L_PORT}$'\n'forward-socks5 / 127.0.0.1:${F_PORT} >> /etc/privoxy/config
+echo listen-address 0.0.0.0:${L_PORT}$'\n'forward-socks5 / 127.0.0.1:${F_PORT} . >> /etc/privoxy/config
 privoxy --no-daemon /etc/privoxy/config
 
 
